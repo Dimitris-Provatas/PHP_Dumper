f618ed1a2e9cbf229cb341822ddb638d02d86257
Dimitris-Provatas <james.provatas@gmail.com>
2020-09-15 17:44:08 +0300

    added the charset and collation for each table so there will be no errors with the dumps, fixed a bug with the GeoData AsText() where the field name was passed as string

--- dumper.php
+++ dumper.php
@@ -63,17 +63,29 @@ if (!empty($user) && !empty($pass) && !empty($host))    // make sure everything 
         }
     }
 
+    $dbNamesArray = ["octane"];
+
     // Loop through the databases
     foreach ($dbNamesArray as $db)
     {
+        $tableConn = new mysqli($host, $user, $pass, $db);      // make the connection to target database
+
+        $getCharset = "SELECT @@character_set_database;";                           // Charset query
+        $getCharsetResult = $tableConn->query($getCharset);                         // perform the query
+        $charset = $getCharsetResult->fetch_assoc()["@@character_set_database"];    // Store the charset
+
+        $getCollation = "SELECT @@collation_database;";                                 // Collation query
+        $getCollationResult = $tableConn->query($getCharset);                           // perform the query
+        $collation = $getCollationResult->fetch_assoc()["@@character_set_database"];    // Store the collation
+
         $dbFileLocation = $_SERVER['DOCUMENT_ROOT'] . "/PHP_Dump/$db.sql";              // store the file path
-        file_put_contents($dbFileLocation, "-- DATABASE: $db\n", FILE_APPEND);          // create the file and append the database name
+        file_put_contents($dbFileLocation, "-- DATABASE: $db\n", FILE_APPEND);          // create the file and append the database name, collation and charset 
+        file_put_contents($dbFileLocation, "-- Collation: $collation -- Charset: $charset --\n", FILE_APPEND);
+        file_put_contents($dbFileLocation, "\n", FILE_APPEND);                          // empty line for readability
         file_put_contents($dbFileLocation, "SET FOREIGN_KEY_CHECKS=0;\n", FILE_APPEND); // set the foreign key checks to 0 so there will be no import errors
         file_put_contents($dbFileLocation, "\n", FILE_APPEND);                          // empty line for readability
 
         $tableNames = [];   // start the tables array as empty
-
-        $tableConn = new mysqli($host, $user, $pass, $db);  // make the connection to target database
 
         $tablesQuery = "SHOW TABLES;";                      // get all tables from the database
         $tablesResult = $tableConn->query($tablesQuery);    // perform the query
@@ -90,7 +102,7 @@ if (!empty($user) && !empty($pass) && !empty($host))    // make sure everything 
         // loop through the tables
         foreach ($tableNames as $tableName)
         {
-            QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db);
+            QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db, $charset, $collation);
         }
 
         $tableConn->close();    // close the table connection (memory optimisation)
@@ -100,14 +112,15 @@ if (!empty($user) && !empty($pass) && !empty($host))    // make sure everything 
 
     $conn->close(); // close the connection to MySQL (memory optimisation)
 
-    die ("<h4>Done with all Databases!</h4>");  // kill the server and inform the user
+    $path = $_SERVER['DOCUMENT_ROOT'] . "/PHP_Dump";                        // get the output path
+    die ("<h4>Done with all Databases!</h4>You can find them at: $path");   // kill the server and inform the user about the path of the files
 }
 // Not all 3 nevessary arguments were given
 else
-    die("<h1 style='text-align: center;'>You need to specify a user, a password and a hostname for the script to connect to MySQL.<br>You can do that in the URL by appending parameters:<br>yoururl/dumper.php?name=[your name]&pass=[your pass]&host=[your DB host]</h1>");
+    die("<h1 style='text-align: center;'>You need to specify a user, a password and a hostname for the script to connect to MySQL.<br>You can do that in the URL by appending parameters:<br>yoururl/dumper.php?user=[your name]&pass=[your pass]&host=[your DB host]</h1>");
 
 // Get field types and create the correct SELECT statement for each table
-function QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db)
+function QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db, $charset, $collation)
 {
     $fieldConn = new mysqli($host, $user, $pass, $db);  // make a new connection for the fields in each table
     $fields= [];        // start the fields array as empty
@@ -126,7 +139,7 @@ function QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db)
     while($row = $fieldResult->fetch_assoc())
     {
         if (strcasecmp($row['Type'], "polygon") == 0 || strcasecmp($row['Type'], "point") == 0 || strcasecmp($row['Type'], "linestring") == 0)
-            array_push($fields, "AsText('" . $row['Field'] . "')"); // if there is a Geodata field, get it as text
+            array_push($fields, "AsText(" . $row['Field'] . ")"); // if there is a Geodata field, get it as text
         else
             array_push($fields, "`" . $row['Field'] . "`");         // else just get the field name
 
@@ -134,16 +147,18 @@ function QueryFields($dbFileLocation, $tableName, $host, $user, $pass, $db)
         array_push($fieldNull, $row['Null']);       // store if the field can be null
     }
 
-    QueryRows($dbFileLocation, $host, $user, $pass, $db, $tableName, $fields, $fieldTypes, $fieldNull);
+    QueryRows($dbFileLocation, $host, $user, $pass, $db, $tableName, $fields, $fieldTypes, $fieldNull, $charset, $collation);
     file_put_contents($dbFileLocation, "\n", FILE_APPEND);  // create an empty line after all table contents 
 
     $fieldConn->close();    // close the connection to the fields (memory optimisation)
 }
 
 // Gets every row from table
-function QueryRows($dbFileLocation, $host, $user, $pass, $db, $table, $fields, $fieldTypes, $fieldNull)
+function QueryRows($dbFileLocation, $host, $user, $pass, $db, $table, $fields, $fieldTypes, $fieldNull, $charset, $collation)
 {
     $dataConn = new mysqli($host, $user, $pass, $db);   // make a new connection for the data in each table
+    $dataConn->set_charset($charset) or die("FAILED TO SET CHARSET TO $charset FOR DATABASE $db ON TABLE $table!"); // set the charset
+    $dataConn->query("SET collation_connection = $collation;"); // set the collation
 
     $dataQuery = "SELECT ";                     // start the query
 
